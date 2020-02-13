5d86cca467a5bc4cfa227d99ee67469e84b687fe
Govind raj <govind.mndy@gmail.com>
2020-02-12 21:25:56 -0500

    Create string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+//program to print a string in java in two different ways
+public class Main
+{
+	public static void main(String[] args) {
+	    String text="govind";
+	    String blank=" ";
+	    String last ="Raj";
+	    String name=text+blank+last;
+		System.out.println(name);   //one way
+		System.out.println("govind"+" " +"Raj"); 
+	}
+}
