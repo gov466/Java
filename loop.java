4cabadec4c5637cefd699c45d5aca7199f4ba6d0
Govind raj <govind.mndy@gmail.com>
2020-02-18 20:24:03 -0500

    Update loop.java

--- loop.java
+++ loop.java
@@ -14,3 +14,16 @@ public class Main
 O/P= Hello 0 to Hello 9
 
 -------------------------------
+//For looop
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	    for(int value=0;value<5;value++)
+	    {
+	   System.out.println("Hello " + value);
+	  
+	    }
+	}
+}
+O/P- Hello 0- Hello 4
