7491cfdc68cb54d06331407a77eab667b3cfe44a
Govind raj <govind.mndy@gmail.com>
2020-02-18 20:21:08 -0500

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,16 @@
+//while loop
+public class Main
+{
+	public static void main(String[] args) {
+	    int value=0;
+	    while(value<10)
+	    {
+	   System.out.println("Hello " + value);
+	   value= value+1;
+	    }
+	}
+}
+
+O/P= Hello 0 to Hello 9
+
+-------------------------------
