3b39dd3d769a837b4efb3e32178be758b4d85fc1
Govind raj <govind.mndy@gmail.com>
2020-02-24 14:23:48 -0500

    Update Array.java

--- Array.java
+++ Array.java
@@ -2,12 +2,12 @@
 public class Main
 {
 	public static void main(String[] args) {
-	   int value=7;
+	   int value=7;				//setting value as 7
 	   int[] values;
 	   values=new int[3];
-	   System.out.println(values[0]);
+	   System.out.println(values[0]);	//printing value in 0th posistion(at present its value is  0)
 	   values[0]=10;
-	   values[1]=20;
+	   values[1]=20;	//giving different values to 0,1,2 posistion
 	   values[2]=30;
 	   System.out.println(values[0]);
 	   System.out.println(values[1]);
