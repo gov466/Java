105f7a8da6bf58b64e790fe5f3c0006573b761b2
Govind raj <govind.mndy@gmail.com>
2020-02-26 21:32:15 -0500

    Create return_value.java

---
+++ return_value.java
@@ -0,0 +1,47 @@
+//Prpogram that returns value to main
+class Person {
+    String name;          //data
+    int age;
+     
+    void speak() {          //methods
+        System.out.println("My name is: " + name);
+    }
+     
+    int calculateYearsToRetirement() {
+        int yearsLeft = 65 - age;
+         
+        return yearsLeft;
+    }
+     
+    int getAge() {      //The age value is returned  to main again
+        return age;
+    }
+     
+    String getName() {
+        return name;
+    }
+}
+ 
+ 
+public class App {
+ 
+    public static void main(String[] args) {
+        Person person1 = new Person();
+         
+        person1.name = "Joe";
+        person1.age = 25;
+         
+        // person1.speak();
+         
+        int years = person1.calculateYearsToRetirement();
+         
+        System.out.println("Years till retirements " + years);
+         
+        int age = person1.getAge();
+        String name = person1.getName();
+         
+        System.out.println("Name is: " + name);
+        System.out.println("Age is: " + age);
+    }
+ 
+}
