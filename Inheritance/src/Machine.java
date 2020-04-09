b892e13063285a29b01582af716624133cdec2e4
Govind raj <govind.mndy@gmail.com>
2020-04-09 16:31:24 -0400

    Update Machine.java

--- Inheritance/src/Machine.java
+++ Inheritance/src/Machine.java
@@ -1,12 +1,13 @@
+//another class called machine
 public class Machine {
      
-    protected String name = "Machine Type 1";
+    protected String name = "Machine Type 1"; //string name is of type protected so can be accessed anywhere from package or child class
      
-    public void start() {
+    public void start() {          //method
         System.out.println("Machine started.");
     }
      
-    public void stop() {
+    public void stop() {      //another methjod
         System.out.println("Machine stopped.");
     }
 }
