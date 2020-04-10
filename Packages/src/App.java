17a92534391aed609b7816451619f2065ed88a90
Govind raj <govind.mndy@gmail.com>
2020-04-10 19:38:01 -0400

    Update App.java

--- Packages/src/App.java
+++ Packages/src/App.java
@@ -1,11 +1,11 @@
-import ocean.Fish;
-import ocean.plants.Seaweed;
+import ocean.Fish;     //fish is a class used here but it resides within a package called ocean so importing from ocean
+import ocean.plants.Seaweed; //seaweed is class imported from package plants which resides as subpackage of ocean
  
 public class App {
  
      
     public static void main(String[] args) {
-        Fish fish = new Fish();
+        Fish fish = new Fish(); //call two classes
         Seaweed weed = new Seaweed();
     }
  
