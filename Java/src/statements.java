fbb0f955c43cda6e552a970855f925c7a9b31c5f
gov466 <govind.mndy@gmail.com>
2021-04-11 11:25:44 -0400

    statements

---
+++ Java/src/statements.java
@@ -0,0 +1,23 @@
+
+public class statements {
+	
+	public static void main(String args[]) {
+		int age=12;
+		
+		if(age>14) {
+			System.out.println("given age is greater than 10");
+		}
+		else if(age<12) {
+			System.out.println("given age is less than 14");
+		}
+		else if(age==12) {
+			System.out.println("given age is equal to 12");
+		}
+		else {
+			System.out.println("none of conditions statisfied");
+		}
+		
+			
+		
+	}
+}
